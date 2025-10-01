b7e6f1a7aad94685d2d24d1461d1578269095e23
Santiago Aramburu <santiburu77@gmail.com>
2025-09-30 23:16:17 -0400

    Comments Routes done

--- controllers/sounds.js
+++ controllers/sounds.js
@@ -2,4 +2,3 @@ const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
 const Sound = require("../models/sound.js");
 const router = express.Router();
-
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.js";
 import usersRouter from "./users.js";
+import commentsRouter from "./comments.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
-router.use("/sounds");
+router.use("/comments", commentsRouter);
 
 export default router;
